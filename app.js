6afdeb374180591ba0a3f9abbb9df3690d4f5177
SebaSacchi <sebastiansacchi@gmail.com>
2026-06-24 11:42:51 -0300

    Primera version Tablero Agencia

---
+++ app.js
@@ -0,0 +1,16 @@
+﻿function actualizarFecha() {
+  const ahora = new Date();
+
+  const opciones = {
+    weekday: "long",
+    day: "2-digit",
+    month: "2-digit",
+    year: "numeric"
+  };
+
+  document.getElementById("fecha").textContent =
+    ahora.toLocaleDateString("es-AR", opciones).toUpperCase();
+}
+
+actualizarFecha();
+setInterval(actualizarFecha, 1000);
